ec6cb6a9ed68ea23ae17177f39a126bfc6ddcaa4
Cadell Singh <cadellsingh9@gmail.com>
2021-04-03 21:55:48 -0400

    dont need main content comp again

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import { useContext } from 'react';
 import { GlobalStyles } from './styles/globalStyling';
-import { MainContent } from './components/MainContent';
-import { Admin } from './components/Admin/Admin';
 import { GlobalProvider } from './context/GlobalState';
+import { Navbar } from './components/Navbar';
+import { Route, Switch } from 'react-router';
+import { Home } from './components/Home';
+import { Classes } from './components/Classes';
+import { Schedule } from './components/Schedule';
+import { Login } from './components/Login';
+import { AllClasses } from './components/Admin/AllClasses';
+import { PrivateRoute } from './components/PrivateRoute';
+import { CreateClass } from './components/Admin/CreateClass';
+import { UpdateClass } from './components/Admin/UpdateClass';
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
       <GlobalProvider>
-        <MainContent />
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/classes" component={Classes} />
+          <Route path="/schedule" component={Schedule} />
+          <Route path="/login" component={Login} />
+          <PrivateRoute path="/editClasses" component={AllClasses} />
+          <PrivateRoute path="/createClass" component={CreateClass} />
+          <PrivateRoute path="/:classID" component={UpdateClass} />
+        </Switch>
       </GlobalProvider>
     </>
   );
