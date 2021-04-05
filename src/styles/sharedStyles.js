7fd40a78d203891593615667842d8396a17a986f
Cadell Singh <cadellsingh9@gmail.com>
2021-04-05 09:33:24 -0400

    extracted and made card component

--- src/styles/sharedStyles.js
+++ src/styles/sharedStyles.js
@@ -7,3 +7,13 @@ export const BackgroundImage = styled.div`
   -o-background-size: cover;
   background-size: cover;
 `;
+
+export const CardContainer = styled.div`
+  background-color: #f5f5f5;
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  padding: 20px;
+`;
