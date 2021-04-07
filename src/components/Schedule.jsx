c6b5cbe8e26dc6812730a1e0e0b303ef38e380a4
Cadell Singh <cadellsingh9@gmail.com>
2021-04-07 18:38:01 -0400

    design changed on schedule component

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,91 +1,120 @@
 import { useContext } from 'react';
 import styled from 'styled-components';
-import swimming from '../images/swimming5.jpg';
 import { GlobalContext } from '../context/GlobalState';
 
-const Layout = styled.div`
-  height: 90vh;
-  background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
-    url(${swimming});
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
+const Container = styled.div`
+  font-size: 2.3rem;
+  width: 90%;
+  margin: 0 auto;
+  color: white;
+  display: grid;
+  grid-template-columns: 1fr;
+  row-gap: 20px;
+  margin-top: 30px;
+  border: 1px solid grey;
+  border-radius: 10px;
 `;
 
-const GridContainer = styled.div`
+const ScheduleContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  text-align: center;
-  gap: 10px;
-  margin-top: 15px;
+  grid-template-columns: 0.8fr 1fr 1fr 1fr;
+  border-bottom: 1px solid grey;
+  padding: 20px 0;
+`;
+
+const Day = styled.div`
+  margin: auto;
 
   & p {
-    background-color: rgba(255, 255, 255, 0.25);
-    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-    backdrop-filter: blur(4px);
-    -webkit-backdrop-filter: blur(4px);
-    border: 1px solid rgba(255, 255, 255, 0.18);
-    padding: 30px;
+    padding: 10px;
+    border-radius: 5px;
   }
 `;
 
-const Titles = styled(GridContainer)`
-  font-size: 20px;
-  margin-bottom: 10px;
-`;
-
-const ScheduleDetails = styled(GridContainer)`
-  font-size: 18px;
-`;
-
-const Filters = styled.div`
+const Classes = styled.div`
   display: flex;
+  flex-direction: column;
+  grid-column: span 3 / auto;
+`;
 
-  & ul {
-    display: flex;
+const ClassDetails = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 20px;
+  font-size: 1.7rem;
+  margin: 5px 0;
+  text-align: center;
+
+  & p {
+    padding: 5px;
   }
 
-  & input {
-    border: none;
-    outline: none;
+  & p:first-child {
+    background-color: #287094;
+    border-radius: 5px;
   }
 `;
+
+const LineBreak = styled.span``;
 
 export const Schedule = () => {
   const { classes } = useContext(GlobalContext);
 
-  const displaySchedule = classes.map((eachClass) => {
-    const { name, time, day } = eachClass;
+  const days = {
+    Monday: [],
+    Tuesday: [],
+    Wednesday: [],
+    Thursday: [],
+    Friday: [],
+  };
 
+  for (const eachClass of classes) {
+    const { name, time, day, instructor } = eachClass;
+    const details = [name, instructor, time];
+
+    switch (day) {
+      case 'Monday':
+        days.Monday.push(details);
+        break;
+      case 'Tuesday':
+        days.Tuesday.push(details);
+        break;
+      case 'Wednesday':
+        days.Wednesday.push(details);
+        break;
+      case 'Thursday':
+        days.Thursday.push(details);
+        break;
+      case 'Friday':
+        days.Friday.push(details);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  const displaySchedule = Object.entries(days).map(([key, value], idx) => {
     return (
-      <>
-        <p>{name}</p>
-        <p>{day}</p>
-        <p>{time}</p>
-      </>
+      <ScheduleContainer>
+        <Day key={idx}>
+          <p>{key}</p>
+        </Day>
+        <Classes>
+          {value.map((details, idx) => {
+            return (
+              <ClassDetails key={idx}>
+                {details.map((detail, idx) => {
+                  return <p key={idx}>{detail}</p>;
+                })}
+              </ClassDetails>
+            );
+          })}
+          <LineBreak />
+        </Classes>
+      </ScheduleContainer>
     );
   });
 
-  return (
-    <Layout>
-      <Filters>
-        <ul>
-          <li>Mon</li>
-          <li>Tues</li>
-          <li>Wed</li>
-          <li>Thur</li>
-          <li>Fri</li>
-        </ul>
-        <input type="text" placeholder="Search..." />
-      </Filters>
-      <Titles>
-        <p>Class Name</p>
-        <p>Day</p>
-        <p>Time</p>
-      </Titles>
-
-      <ScheduleDetails>{displaySchedule}</ScheduleDetails>
-    </Layout>
-  );
+  return <Container>{displaySchedule}</Container>;
 };
